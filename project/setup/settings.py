af50aa41123a4e8e79898d42fb0ab1b47b9d8310
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-09-06 02:04:38 +0100

    feat: Integrated Sentry for API Management

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -1,8 +1,23 @@
 from pathlib import Path
 import environ
+import sentry_sdk
+
 
 env = environ.Env()
 environ.Env.read_env()
+
+
+# Sentry Setup
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- project/setup/urls.py
+++ project/setup/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+

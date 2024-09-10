e37129fda6dbe1f8f18d9322ac056b48c8dbb907
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-09-10 02:00:59 +0100

    deployment: Deployment to AWS ELB Complete, Served Staticfiles using Whitenoise

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import environ
 import sentry_sdk
+import os
 
 
 env = environ.Env()
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -126,7 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
